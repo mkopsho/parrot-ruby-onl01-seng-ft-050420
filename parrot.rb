37f17bb9d4f3bb871f0df8008dc966ea77f392a6
Michael Kopsho <michael.kopsho@gmail.com>
2020-05-04 18:05:10 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase)
-  puts "Squawk!"
-  "Squawk!"
+def parrot(phrase = "Squawk!")
   puts phrase
   phrase
 end
